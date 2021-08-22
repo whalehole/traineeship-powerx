1f1d78ba934aeb3793a7d5acbcdff4e2535e739c
whalehole <68460417+whalehole@users.noreply.github.com>
2021-08-22 13:12:45 +0800

    update

---
+++ frontend/marketplace-on-day3/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Marketplace from './pages/marketplace';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Marketplace />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ frontend/marketplace-on-day3/src/pages/marketplace.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+
+
+
+
+
+export default function Marketplace() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
