002d79c40d7b103eaf83d0f0b20e3c5d47d73844
whalehole <68460417+whalehole@users.noreply.github.com>
2021-09-21 13:36:19 +0800

    removed getcomic script cuz not in use

--- projects/comicapp/getComic.js
+++
@@ -1,8 +0,0 @@
-export const getComic = (id) => 
-    fetch(`https://intro-to-js-playground.vercel.app/api/xkcd-comics/${i}`)
-    .then(res=>{
-        if (res.ok) {
-            return res.json();
-        }
-        throw new Error(res.statusText);
-    })
